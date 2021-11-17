14c5495e522873b154e17de7ea0836bfd80f5ef0
JafarAli360 <61019621+JafarAli360@users.noreply.github.com>
2021-11-17 12:06:38 +0700

    Create Tiket_konser.blade.php

---
+++ Tiket_konser.blade.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="{{asset ('css/Gaya.css')}}">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+   
+    <title>Tiket</title>
+  </head>
+  <body>
+    <p align ="center"><b><font size="10" color="Black">Tiket Film</font></b></p>
+    <div class="card" style="width: 14rem;">
+        <img src="{{asset ('/img/3.jpg')}}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Godzilla vs Kong</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+
+      <div class="card" style="width: 14rem;">
+        <img src="{{asset ('/img/3.jpg')}}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Godzilla vs Kong</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+
+      <div class="card" style="width: 14rem;">
+        <img src="{{asset ('/img/3.jpg')}}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Godzilla vs Kong</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+
+      <div class="card" style="width: 14rem;">
+        <img src="{{asset ('/img/3.jpg')}}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Godzilla vs Kong</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+
+      <div class="card" style="width: 14rem;">
+        <img src="{{asset ('/img/3.jpg')}}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Godzilla vs Kong</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+
+      <div class="card" style="width: 14rem;">
+        <img src="{{asset ('/img/3.jpg')}}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Godzilla vs Kong</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+
+      <div class="feature">
+        <div class="container">
+          <div class="feature-box row">
+            <div class="list col-sm-6 col-md-6 col-lg-6">
+              <div class="feature-image placeholder-image">
+                <img alt="Simple Profile" src="https://www.tiket.com/home-ms/assets/simple-profile.png" style="width:82px"/>
+              </div>
+
+              <div class="feature-info">
+                <h3 class="title">Simple</h3>
+                <p class="subtitle">Pesan lebih cepat, isi data penumpang dengan sekali klik.</p>
+              </div>
+            </div>
+
+            <div class="list col-sm-6 col-md-6 col-lg-6">
+              <div class="feature-image placeholder-image">
+                <img alt="Simple Reschedule" src="https://www.tiket.com/home-ms/assets/simple-reschedule.png" style="width:82px"/>
+              </div>
+              <div class="feature-info">
+                <h3 class="title">Mudah</h3>
+                <p class="subtitle">Memudahkan kamu mengatur ulang penerbangan.</p>
+              </div>
+            </div>
+
+            <div class="list col-sm-6 col-md-6 col-lg-6">
+              <div class="feature-image placeholder-image">
+                <img alt="Simple Booking Process" src="https://s-light.tiket.photos/t/01E25EBZS3W0FY9GTG6C42E1SE/original/home-desktop/2021/08/06/ff80aee2-00ae-4d33-af83-59dd73e3762d-1628237435194-527ed12e0af74bc39c095034fa1ea66a.png" style="width:82px"/>
+              </div>
+              <div class="feature-info">
+                <h3 class="title"></h3>
+                <p class="subtitle">Pemesanan tanpa ribet di mana pun dan kapan pun.</p>
+              </div>
+            </div>
+
+            <div class="list col-sm-6 col-md-6 col-lg-6">
+              <div class="feature-image placeholder-image">
+                <img alt="Simple Refund" src="https://www.tiket.com/home-ms/assets/simple-refund.png" style="width:79px"/>
+              </div>
+              <div class="feature-info">
+                <h3 class="title">Murah</h3>
+                <p class="subtitle">Refund tiket tanpa ribet dari aplikasi maupun website.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  </body>
+</html>
+
+<div class="">
+  <div class="loyalty-spot">
+    <div class="container">
+      <div class="head">
+        <h3 class="title-text">Fitur Baru untuk Liburan Lebih Aman!</h3>
+        <h3 class="subtitle-text">Dari tiket CLEAN untuk memastikan kebersihan, sampai tiket FLEXI yang bisa dipakai sampai 1 tahun kedepan.</h3>
+      </div>
+      <div class="row">
+        <div class="col-xs-3 four-row-buttons">
+          <a href="https://www.tiket.com/info/tiket-flexi" target="_blank" rel="noreferrer noopener" data-click="{&quot;event&quot;:&quot;click&quot;,&quot;eventCategory&quot;:&quot;homeContent&quot;,&quot;eventLabel&quot;:&quot;ID;5f0eb0d2ff811c04d93a3f3b;Fitur Baru untuk Liburan Lebih Aman!;wst:6;Dari tiket CLEAN untuk memastikan kebersihan, sampai tiket FLEXI yang bisa dipakai sampai 1 tahun kedepan.;wsst:16;FOUR_ROW_BUTTON;sp:1;tiket FLEXI Hotel;wct:3;Fleksibel tentukan waktu yang tepat untuk menginap di hotel favoritmu.;wcst:10;https://www.tiket.com/info/tiket-flexi;cp:1&quot;}">
+            <div class="list" style="background-image:url(https://s-light.tiket.photos/t/01E25EBZS3W0FY9GTG6C42E1SE/original/home/2020/09/28/4e8075bc-662a-4055-a0ed-71d40018f525-1601278341856-c8149f87067ac62653dd10df7231515c.jpg)">
+            </div>
+          </a>
+        </div>
+
+        <div class="col-xs-3 four-row-buttons">
+          <a href="https://www.tiket.com/info/tiket-flexi-to-do" target="_blank" rel="noreferrer noopener" data-click="{&quot;event&quot;:&quot;click&quot;,&quot;eventCategory&quot;:&quot;homeContent&quot;,&quot;eventLabel&quot;:&quot;ID;5f0eb0d2ff811c04d93a3f3b;Fitur Baru untuk Liburan Lebih Aman!;wst:6;Dari tiket CLEAN untuk memastikan kebersihan, sampai tiket FLEXI yang bisa dipakai sampai 1 tahun kedepan.;wsst:16;FOUR_ROW_BUTTON;sp:1;tiket FLEXI To Do;wct:4;Fleksibel tentukan waktu untuk bisa menikmati aktivitas favoritmu.;wcst:8;https://www.tiket.com/info/tiket-flexi-to-do;cp:2&quot;}">
+            <div class="list" style="background-image:url(https://s-light.tiket.photos/t/01E25EBZS3W0FY9GTG6C42E1SE/original/home/2020/09/28/2847aa1b-7e68-42c9-b59b-7025e79723dd-1601278359707-9b80f43511f7562d2db15fcf224b5b8c.jpg)">
+            </div>
+          </a>
+        </div>
+
+        <div class="col-xs-3 four-row-buttons">
+          <a href="https://www.tiket.com/info/tiket-clean" target="_blank" rel="noreferrer noopener" data-click="{&quot;event&quot;:&quot;click&quot;,&quot;eventCategory&quot;:&quot;homeContent&quot;,&quot;eventLabel&quot;:&quot;ID;5f0eb0d2ff811c04d93a3f3b;Fitur Baru untuk Liburan Lebih Aman!;wst:6;Dari tiket CLEAN untuk memastikan kebersihan, sampai tiket FLEXI yang bisa dipakai sampai 1 tahun kedepan.;wsst:16;FOUR_ROW_BUTTON;sp:1;tiket CLEAN;wct:2;Semakin aman dan nyaman dengan protokol kebersihan dari tiket CLEAN.;wcst:10;https://www.tiket.com/info/tiket-clean;cp:3&quot;}">
+            <div class="list" style="background-image:url(https://s-light.tiket.photos/t/01E25EBZS3W0FY9GTG6C42E1SE/original/home/2020/09/28/2665baaa-57cb-4d02-991b-3f1b622f98fb-1601278375646-b7dda288704319ea992185c1f9ba8ae4.jpg)">
+            </div>
+          </a>
+        </div>
+
+        <div class="col-xs-3 four-row-buttons">
+          <a href="https://www.tiket.com/info/multi-insurance" target="_blank" rel="noreferrer noopener" data-click="{&quot;event&quot;:&quot;click&quot;,&quot;eventCategory&quot;:&quot;homeContent&quot;,&quot;eventLabel&quot;:&quot;ID;5f0eb0d2ff811c04d93a3f3b;Fitur Baru untuk Liburan Lebih Aman!;wst:6;Dari tiket CLEAN untuk memastikan kebersihan, sampai tiket FLEXI yang bisa dipakai sampai 1 tahun kedepan.;wsst:16;FOUR_ROW_BUTTON;sp:1;tiket Multi-Insurance;wct:2;Gunakan tiket Multi-Insurance untuk perjalanan yang lebih nyaman.;wcst:8;https://www.tiket.com/info/multi-insurance;cp:4&quot;}">
+            <div class="list" style="background-image:url(https://s-light.tiket.photos/t/01E25EBZS3W0FY9GTG6C42E1SE/original/home/2020/09/28/028dd8ac-b14e-41ac-96b6-3807e077e37a-1601278390376-e072679e768e01880c6e7a38c9ed092f.jpg)"></div></a></div></div><div class="more-button"><a class="button-secondary" href="https://www.tiket.com/info/tiket-clean">Info selengkapnya</a>
+            </div>
+          </div>
+        </div>
+      </div>
